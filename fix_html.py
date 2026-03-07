8ea8eaf1f46e0b51cb6cfea5551d630a33f7fa65
Shespito1 <shespito1@GMAIL.COM>
2026-03-07 12:31:03 -0400

    Deploying front-end to GitHub Pages and untracking local logic

--- fix_html.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('public/index.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = re.sub(r'(this\.socket\.on\("atualizarMao".*?\n[\s]*)+', 'this.socket.on("atualizarMao", (data) => { this.mao = data.mao; });\n                ', text)
-
-with open('public/index.html', 'w', encoding='utf-8') as f:
-    f.write(text)
